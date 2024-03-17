e43492fdb0fc0a7f6c7234bb24399c501b7a6dbd
kang-theo <theo.cy.kang@gmail.com>
2024-03-17 16:53:55 +0800

    Head component deprecated, instead using Metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
-import Head from '@/app/about/head'
 
 export default function About() {
-  throw new Error('Error testing!');
+  // throw new Error('Error testing!');
   return (
     <>
-      {/* <Head></Head> */}
+      {/* <Head /> */}
       <h1>About</h1>
       <Link href="/">Link to Home page</Link>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import styles from "./page.module.css"
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1>Home page</h1>
       <Link href="/about">Go to About page</Link>
     </main>
   );
