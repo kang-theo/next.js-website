e5d6437d1f1cdddd805990481a18c0681a7943fc
kang-theo <theo.cy.kang@gmail.com>
2024-03-17 16:25:38 +0800

    Head(meta), Loading, Error boundary components(error would not cause app crashed)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from '@/app/about/head'
 
 export default function About() {
+  throw new Error('Error testing!');
   return (
     <>
+      {/* <Head></Head> */}
       <h1>About</h1>
       <Link href="/">Link to Home page</Link>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Hello World!</h1>
       <Link href="/about">Go to About page</Link>
     </main>
