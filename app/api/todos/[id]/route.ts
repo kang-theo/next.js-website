bea556b9a04841832c080195d223a27ab6e8c67c
kang-theo <theo.cy.kang@gmail.com>
2024-03-18 16:36:05 +0800

    Next.js way to pass id param

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -2,10 +2,16 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
 // postman request http://localhost:3000/api/todos/199
 // id is from request url
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+export async function GET(request: Request, { params: { id } }: Props) {
+  // const id = request.url.slice(request.url.lastIndexOf("/") + 1);
 
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
