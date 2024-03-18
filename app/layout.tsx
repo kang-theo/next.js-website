11cae605f68746d5f20d10c27fcf7698bd371af9
kang-theo <theo.cy.kang@gmail.com>
2024-03-18 08:20:31 +0800

    [Tailwind-css] enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <nav>
-          <h1>My navbar</h1>
-        </nav>
+      <body className="bg-slate-800">
+        <Navbar />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Link from "next/link";
-import styles from "./page.module.css"
 
 export default function Home() {
   return (
