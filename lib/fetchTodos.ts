47abb0aa43c3817b11d9e2f4e80ed4a49e320620
kang-theo <theo.cy.kang@gmail.com>
2024-03-19 19:53:01 +0800

    Comment out todo fetching for needing JSON server

--- lib/fetchTodos.ts
+++ lib/fetchTodos.ts
@@ -1,8 +1,8 @@
-
 export default async function fetchTodos() {
-    const res = await fetch(`http://127.0.0.1:3500/todos`)
+  // npx json-server -w db.json -p 3500 -h 127.0.0.1
+  //   const res = await fetch(`http://127.0.0.1:3500/todos`);
+  //   const todos: Todo[] = await res.json();
+  const todos: Todo[] = [];
 
-    const todos: Todo[] = await res.json()
-
-    return todos
+  return todos;
 }
