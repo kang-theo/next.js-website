6fbdee6e20feefdb270346cfea58b44db31f7f34
kang-theo <theo.cy.kang@gmail.com>
2024-03-19 13:34:19 +0800

    Dynamic post pages

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,23 +1,33 @@
 import getFormattedDate from "@/lib/getFormattedDate"
-import { getSortedPostsData, getPostData } from "@/lib/posts"
+import { getPostsMeta, getPostByName } from "@/lib/posts"
 import { notFound } from "next/navigation"
 import Link from "next/link"
 
+// In node_modules/highligh.j, colored code blocks
+import 'highlight.js/styles/github-dark.css'
+
+export const revalidate = 86400
+
+type Props = {
+  params: {
+    postId: string
+  }
+}
+
 // SSR -> SSG
-export function generateStaticParams() {
-  const posts = getSortedPostsData()
+export async function generateStaticParams() {
+  const posts = await getPostsMeta() //deduped!
+
+  if (!posts) return []
 
   return posts.map((post) => ({
     postId: post.id
   }))
 }
 
-export function generateMetadata({ params }: { params: { postId: string } }) {
+export async function generateMetadata({ params: { postId } }: Props) {
 
-  const posts = getSortedPostsData()
-  const { postId } = params
-
-  const post = posts.find(post => post.id === postId)
+  const post = await getPostByName(`${postId}.mdx`) //deduped!
 
   if (!post) {
     return {
@@ -26,33 +36,42 @@ export function generateMetadata({ params }: { params: { postId: string } }) {
   }
 
   return {
-    title: post.title,
+    title: post.meta.title,
   }
 }
 
-export default async function Post({ params }: { params: { postId: string } }) {
+export default async function Post({ params: { postId } }: Props) {
 
-  const posts = getSortedPostsData()
-  const { postId } = params
+  const post = await getPostByName(`${postId}.mdx`) //deduped!
 
-  if (!posts.find(post => post.id === postId)) notFound()
+  if (!post) notFound()
 
-  const { title, date, contentHtml } = await getPostData(postId)
+  const { meta, content } = post
 
-  const pubDate = getFormattedDate(date)
+  const pubDate = getFormattedDate(meta.date)
+
+  const tags = meta.tags.map((tag, i) => (
+    <Link key={i} href={`/tags/${tag}`}>{tag}</Link>
+  ))
 
   return (
-    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
-      <p className="mt-0">
+    <>
+      <h2 className="text-3xl mt-4 mb-0">{meta.title}</h2>
+      <p className="mt-0 text-sm">
         {pubDate}
       </p>
       <article>
-        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-        <p>
-          <Link href="/">← Back to home</Link>
-        </p>
+        {content}
       </article>
-    </main>
+      <section>
+        <h3>Related:</h3>
+        <div className="flex flex-row gap-4">
+          {tags}
+        </div>
+      </section>
+      <p className="mb-10">
+        <Link href="/">← Back to home</Link>
+      </p>
+    </>
   )
 }
